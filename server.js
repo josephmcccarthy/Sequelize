cbe0c16380770e932e7df7d4d3597b95217b89f1
joseph mccarthy <jem2018@umd.edu>
2021-03-25 17:56:51 -0400

    server update

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import apiRoutes from './routes/apiRoutes.js';
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+const staticFolder = 'public';
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
